112c584ce06a6849e4c615e835819d4590b95ad5
Abhishek Chakravarti <abhishek@taranjali.org>
2019-02-05 10:51:53 +0530

    Add sol_tsuite_register() test case #1
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/test.t.c
+++ test/test.t.c
@@ -58,12 +58,21 @@
 
 
 /*
- *      log_dummy() - dummy test suite logging callback
+ *      DESC_REGISTER_01 - description for sol_tsuite_register() unit test #1
+ */
+#define DESC_REGISTER_01 "sol_tsuite_register() should throw SOL_ERNO_PTR" \
+                         " when passed a null pointer for @tsuite"
+
+
+
+
+/*
+ *      tlog_dummy() - dummy test suite logging callback
  */
 static void
-log_dummy(char     const *desc, /* test case description */
-          sol_erno const erno   /* test case error code  */
-         )
+tlog_dummy(char     const *desc, /* test case description */
+           sol_erno const erno   /* test case error code  */
+          )
 {
 }
 
@@ -98,7 +107,7 @@ test_init2_01(void)
 {
 SOL_TRY:
                 /* sol_tsuite_init2() should fail with SOL_ERNO_PTR */
-        sol_try (sol_tsuite_init2 (0, log_dummy));
+        sol_try (sol_tsuite_init2 (0, tlog_dummy));
 
 SOL_CATCH:
                 /* check test condition described by DESC_INIT2_01 */
@@ -133,6 +142,29 @@ SOL_CATCH:
 
 
 /*
+ *      test_register_01() - sol_tsuite_register() unit test #1
+ */
+static sol_erno
+test_register_01(void)
+{
+        auto sol_tsuite ts; /* dummy test suite */
+
+SOL_TRY:
+                /* sol_tsuite_register() should fail with SOL_ERNO_PTR */
+        sol_try (sol_tsuite_init2    (&ts, tlog_dummy));
+        sol_try (sol_tsuite_register (0, test_init_01, "Dummy"));
+
+SOL_CATCH:
+                /* check test condition described by DESC_REGISTER_01 */
+        return SOL_ERNO_PTR == sol_erno_now ()
+               ? SOL_ERNO_NULL
+               : SOL_ERNO_TEST;
+}
+
+
+
+
+/*
  *      __sol_tsuite_test() - declared in sol/test/suite.h
  */
 extern sol_erno
@@ -152,9 +184,10 @@ SOL_TRY:
         sol_try (sol_tsuite_init2 (ts, log));
 
                 /* register test cases */
-        sol_try (sol_tsuite_register (ts, test_init_01,  DESC_INIT_01));
-        sol_try (sol_tsuite_register (ts, test_init2_01, DESC_INIT2_01));
-        sol_try (sol_tsuite_register (ts, test_init2_02, DESC_INIT2_02));
+        sol_try (sol_tsuite_register (ts, test_init_01,     DESC_INIT_01));
+        sol_try (sol_tsuite_register (ts, test_init2_01,    DESC_INIT2_01));
+        sol_try (sol_tsuite_register (ts, test_init2_02,    DESC_INIT2_02));
+        sol_try (sol_tsuite_register (ts, test_register_01, DESC_REGISTER_01));
 
                 /* execute test cases */
         sol_try (sol_tsuite_exec (ts));
