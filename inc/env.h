ab34ed4060ce2b9a5a6873d589b823cae5bc5cbd
Abhishek Chakravarti <abhishek@taranjali.org>
2019-02-13 13:38:19 +0530

    Add detail comments
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- inc/env.h
+++ inc/env.h
@@ -36,6 +36,17 @@
  *      SOL_ENV_CC - enumerates supported C compilers
  *        - SOL_ENV_CC_GNUC : GNU C or GNU C compatible compiler
  *        - SOL_ENV_CC_CLANG: CLang front-end of LLVM compiler
+ *
+ *      The SOL_ENV_CC type enumerates the C compilers that are supported by the
+ *      Sol Library. The constants enumerated by this type are returned by the
+ *      sol_env_cc() macro (defined below) in order to indicate the C compiler
+ *      being used at compile-time.
+ *
+ *      Currently, only GNU C and CLang compilers are supported directly, and
+ *      GNU C compatible compilers indirectly. This is expected to be adequate
+ *      as far as portability is concerned, as GNU C is the most widely ported C
+ *      compiler. However, in future, support for other compilers, including
+ *      MSVC, *may* be introduced if there is adequate reason to do so.
  */
 typedef enum __SOL_ENV_CC {
         SOL_ENV_CC_GNUC,
@@ -53,6 +64,18 @@ typedef enum __SOL_ENV_CC {
  *        - SOL_ENV_STDC_C99: C99 standard
  *        - SOL_ENV_STDC_C11: C11 standard
  *        - SOL_ENV_STDC_C18: C18 standard
+ *
+ *      The SOL_ENV_STDC type enumerates the C language standards that are
+ *      supported by the Sol Library. The constants enumerated by this type are
+ *      used by the sol_env_stdc() macro (defined below) in order to indicate
+ *      the compile-time C standard being used.
+ *
+ *      The Sol Library is designed to be backward-compatible with the C89
+ *      standard, while taking advantage of some of the new features introduced
+ *      by the modern standards. Backward-compatiblity with the C89 standard
+ *      ensures maximum portability; dialects predating the C89 standard are not
+ *      considered, as they are fairly archaic and limited only to deprecated
+ *      compilers.
  */
 typedef enum __SOL_ENV_STDC {
         SOL_ENV_STDC_C89,
@@ -76,7 +99,17 @@ typedef enum __SOL_ENV_STDC {
  *        - SOL_ENV_HOST_AIX    : IBM AIX
  *        - SOL_ENV_HOST_IOS    : Apple iOS
  *        - SOL_ENV_HOST_OSX    : Apple OSX
- *        - SOL_ENV_HOST_SOLARIS: Oracle Solaris, Open Indiana
+ *        - SOL_ENV_HOST_SOLARIS: Oracle Solaris / Open Indiana
+ *
+ *      The SOL_ENV_HOST type enumerates the host platforms that are supported
+ *      by the Sol Library. The constants enumerated by this type are returned
+ *      by the sol_env_host() macro (defined below) in order to indicate the
+ *      compile-time host platform.
+ *
+ *      The Sol Library has been designed to support both freestanding and POSIX
+ *      compliant hosted platforms; however, as of now, only the Linux platform
+ *      has been tested, and support for the others being assumed based on these
+ *      tests.
  */
 typedef enum __SOL_ENV_HOST {
         SOL_ENV_HOST_ANDROID,
@@ -98,6 +131,16 @@ typedef enum __SOL_ENV_HOST {
  *        - SOL_ENV_ARCH_X68  : 32-bit x86 processor
  *        - SOL_ENV_ARCH_AMD64: 64-bit x86_64 processor
  *        - SOL_ENV_ARCH_IA64 : 64-bit Itanium processor
+ *
+ *      The SOL_ENV_ARCH type enumerates the CPU architectures supported by the
+ *      Sol Library. The constants enumerated by this type are returned by the
+ *      sol_env_host() macro (defined below) in order to indicate the processor
+ *      architecture at compile-time.
+ *
+ *      The Sol Library currently supports the 32-bit x86 family of processors,
+ *      and the 64-bit x86_64 and Itanium family of processors. The Sol Library
+ *      has been tested on the x86_64 architecture, and support for the others
+ *      is assumed based on these tests.
  */
 typedef enum __SOL_ENV_ARCH {
         SOL_ENV_ARCH_X86,
@@ -110,6 +153,15 @@ typedef enum __SOL_ENV_ARCH {
 
 /*
  *      sol_env_cc() - determines C compiler at compile-time
+ *
+ *      The sol_env_cc() macro determines at compile-time the C compiler that is
+ *      being used. This macro returns one of the constants enumerated by the
+ *      SOL_ENV_CC type as appropriate. A compile-time error is raised in case
+ *      an unsupported compiler is detected.
+ *
+ *      Return:
+ *        - SOL_ENV_CC_GNUC if GNU C or GNU C compatible compiler detected
+ *        - SOL_ENV_CC_CLANG if CLang compiler detected
  */
 #if (defined __GNUC__)
 #       define sol_env_cc() SOL_ENV_CC_GNUC
@@ -124,6 +176,20 @@ typedef enum __SOL_ENV_ARCH {
 
 /*
  *      sol_env_stdc() - determines compile-time C language standard
+ *
+ *      The sol_env_stdc() macro determines at compile-time the C language
+ *      standard that is being used. This macro returns one of the constants
+ *      enumerated by the SOL_ENV_STDC type as appropriate. A compile-time error
+ *      is raised in case an unsupported standard is detected.
+ *
+ *      Return:
+ *        - SOL_ENV_STDC_C89 if C89 standard detected
+ *        - SOL_ENV_STDC_C90 if C90 standard detected
+ *        - SOL_ENV_STDC_C94 if C94 standard detected
+ *        - SOL_ENV_STDC_C99 if C99 standard detected
+ *        - SOL_ENV_STDC_C11 if C11 standard detected
+ *        - SOL_ENV_STDC_C18 if C18 standard detected
+ *
  */
 #if (defined __STDC__)
 #       if (defined __STDC_VERSION__)
@@ -150,6 +216,22 @@ typedef enum __SOL_ENV_ARCH {
 
 /*
  *      sol_env_host() - determines compile-time host platform
+ *
+ *      The sol_env_host() macro determines at compile-time the host platform
+ *      that is being used. This macro returns one of the constants enumerated
+ *      by the SOL_ENV_HOST type as appropriate. A compile-time error is raised
+ *      in case an unsupported host is detected.
+ *
+ *      Return:
+ *        - SOL_ENV_HOST_ANDROID if Android detected
+ *        - SOL_ENV_HOST_LINUX if other Linux flavour detected
+ *        - SOL_ENV_HOST_CYGWIN if Cygwin on Microsoft Windows detected
+ *        - SOL_ENV_HOST_BSD if a BSD variant detected
+ *        - SOL_ENV_HOST_HPUX if HP-UX detected
+ *        - SOL_ENV_HOST_AIX if IBM AIX detected
+ *        - SOL_ENV_HOST_IOS if Apple iOS detected
+ *        - SOL_ENV_HOST_OSX if Apple OSX detected
+ *        - SOL_ENV_HOST_SOLARIS if Oracle Solaris / Open Indiana detected
  */
 #if (defined __CYGWIN__)
 #       define sol_env_host() SOL_ENV_HOST_CYGWIN
@@ -184,6 +266,16 @@ typedef enum __SOL_ENV_ARCH {
 
 /*
  *      sol_env_arch() - determines compile-time CPU architecture
+ *
+ *      The sol_env_arch() macro determines at compile-time the processor
+ *      architecture that is being used. This macro returns one of the constants
+ *      enumerated by the SOL_ENV_ARCH type as appropriate. A compile-time error
+ *      is raised in case an unsupported architecture is detected.
+ *
+ *      Return:
+ *        - SOL_ENV_ARCH_X68 if 32-bit x86 processor detected
+ *        - SOL_ENV_ARCH_AMD64 if 64-bit x86_64 processor detected
+ *        - SOL_ENV_ARCH_IA64 if 64-bit Itanium processor detected
  */
 #if (defined __amd64__ || defined __amd64                          \
      || defined __x86_64__  || defined __x86_64)
@@ -202,6 +294,15 @@ typedef enum __SOL_ENV_ARCH {
 
 /*
  *      sol_env_wordsz() - determines compile-time native word size
+ *
+ *      The sol_env_wordsz() macro determines the native word size of the
+ *      processor at compile-time. This macro supports only the architectures
+ *      defined by the SOL_ENV_ARCH type, with a compile-time error being thrown
+ *      for other processors.
+ *
+ *      Return:
+ *        - 32 if 32-bit processor detected
+ *        - 64 if 64-bit processor detected
  */
 #if (SOL_ENV_ARCH_X86 == sol_env_arch())
 #       define sol_env_wordsz() 32
