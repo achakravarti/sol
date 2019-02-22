a95470c209fb438a0e8d6fc745946ac3d43cc96e
Abhishek Chakravarti <abhishek@taranjali.org>
2019-02-22 17:20:01 +0530

    Implement sol_ptr_copy()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/ptr.c
+++ src/ptr.c
@@ -30,6 +30,7 @@
 #include "../inc/ptr.h"
 #if (sol_env_host() != SOL_ENV_HOST_NONE)
 #       include <stdlib.h>
+#       include <string.h>
 #endif
 
 
@@ -39,14 +40,27 @@ extern sol_erno sol_ptr_new(sol_ptr **ptr,
                             const size_t sz)
 {
 SOL_TRY:
-        #if (SOL_ENV_HOST_NONE == sol_env_host())
-        #       error "[!] sol_ptr_new() error: malloc() not available"
-        #endif
+        sol_assert (ptr && !*ptr, SOL_ERNO_PTR);
+        sol_assert (sz, SOL_ERNO_RANGE);
 
-        sol_assert (ptr && !*ptr, SOL_ERNO_NULL);
-        sol_assert (sz, SOL_ERNO_NULL);
+        sol_assert ((*ptr = malloc(sz)), SOL_ERNO_HEAP);
 
-        sol_assert ((*ptr = malloc(sz)), SOL_ERNO_NULL);
+SOL_CATCH:
+        sol_throw();
+}
+
+
+
+
+extern sol_erno sol_ptr_copy(sol_ptr **ptr,
+                             const sol_ptr *src,
+                             const size_t sz)
+{
+SOL_TRY:
+        sol_assert (ptr && *ptr && src, SOL_ERNO_PTR);
+        sol_assert (sz, SOL_ERNO_RANGE);
+
+        sol_assert ((*ptr = memmove(*ptr, src, sz)), SOL_ERNO_HEAP);
 
 SOL_CATCH:
         sol_throw();
