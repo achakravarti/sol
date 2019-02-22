d63c8b7d5dd3ed292a30069371916cbb63a9a24a
Abhishek Chakravarti <abhishek@taranjali.org>
2019-02-22 14:47:33 +0530

    Add header guard
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ inc/ptr.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ *                           SOL LIBRARY v0.1.0+41
+ *
+ * File: sol/inc/ptr.h
+ *
+ * Description:
+ *      This file is part of the API of the Sol Library. It declares the
+ *      interface of the pointer module.
+ *
+ * Authors:
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *
+ * Copyright:
+ *      (c) 2019 Abhishek Chakravarti
+ *      <abhishek@taranjali.org>
+ *
+ * License:
+ *      Released under the GNU General Public License version 3 (GPLv3)
+ *      <http://opensource.org/licenses/GPL-3.0>. See the accompanying LICENSE
+ *      file for complete licensing details.
+ *
+ *      BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT
+ *      YOU HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT THEM.
+ ******************************************************************************/
+
+
+
+
+#if (!defined __SOL_POINTER_MODULE)
+#define __SOL_POINTER_MODULE
+
+
+
+
+#endif /* (!defined __SOL_POINTER_MODULE) */
+
+
+
+
+/******************************************************************************
+ *                                    EOF
+ *          Built on hyperion [Tue Jan 29 02:37:24 UTC 2019]
+ ******************************************************************************/
+
