0c543a77975f7528925db91461bd5149191fab82
Abhishek Chakravarti <abhishek@taranjali.org>
2019-02-26 08:53:37 +0530

    Refactor exception handling module unit tests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/ts-error.c
+++ test/ts-error.c
@@ -33,55 +33,9 @@
 
 
 /*
- *      DESC_ASSERT_01 - description for sol_assert() unit test #1
+ *      mock_assertpass() - simulates success of sol_assert()
  */
-#define DESC_ASSERT_01 "sol_assert() must not throw an error for a" \
-                       " predicate that evaluates to true"
-
-
-
-
-/*
- *      DESC_ASSERT_02 - description for sol_assert() unit test #2
- */
-#define DESC_ASSERT_02 "sol_assert() must throw an error for a" \
-                       " predicate that evaluates to false"
-
-
-
-
-/*
- *      DESC_TRY_01 - description for sol_try() unit test #1
- */
-#define DESC_TRY_01 "sol_try() must not throw an error for a function that" \
-                    " returns a SOL_ERNO_NULL error code"
-
-
-
-
-/*
- *      DESC_TRY_02 - description for sol_try() unit test #2
- */
-#define DESC_TRY_02 "sol_try() must throw an error for a function that" \
-                    " returns an error code other than SOL_ERNO_NULL"
-
-
-
-
-/*
- *      DESC_NOW_01 - description for sol_erno_get() unit test #1
- */
-#define DESC_NOW_01 "sol_erno_get() must return the current error code that" \
-                    " was thrown by the last calling function"
-
-
-
-
-/*
- *      assert_pass() - simulates success of sol_assert()
- */
-static sol_erno
-assert_pass(void)
+static sol_erno mock_assertpass(void)
 {
 SOL_TRY:
                 /* this condition will always pass */
@@ -90,17 +44,16 @@ SOL_TRY:
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      assert_fail() - simulates failure of sol_assert()
+ *      mock_assertfail() - simulates failure of sol_assert()
  */
-static sol_erno
-assert_fail(void)
+static sol_erno mock_assertfail(void)
 {
 SOL_TRY:
                 /* this condition will always fail */
@@ -109,142 +62,146 @@ SOL_TRY:
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      try_pass() - simulates success of sol_try()
+ *      mock_trypass() - simulates success of sol_try()
  */
-static sol_erno
-try_pass(void)
+static sol_erno mock_trypass(void)
 {
 SOL_TRY:
-                /* assert_pass() is guaranteed to succeed */
-        sol_try (assert_pass ());
+                /* mock_assertpass() is guaranteed to succeed */
+        sol_try (mock_assertpass ());
 
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      try_fail() - simulates failure of sol_try()
+ *      mock_tryfail() - simulates failure of sol_try()
  */
-static sol_erno
-try_fail(void)
+static sol_erno mock_tryfail(void)
 {
 SOL_TRY:
-                /* assert_fail() is guaranteed to fail */
-        sol_try (assert_fail ());
+                /* mock_assertfail() is guaranteed to fail */
+        sol_try (mock_assertfail ());
 
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      test_assert_01() - sol_assert() unit test #1
+ *      test_assert1() - sol_assert() unit test #1
  */
-static sol_erno
-test_assert_01(void)
+static sol_erno test_assert1(void)
 {
+        #define DESC_ASSERT1 "sol_assert() must not throw an error for a" \
+                          " predicate that evaluates to true"
 SOL_TRY:
-                /* check test condition described by DESC_ASSERT_01 */
-        sol_assert (!assert_pass (), SOL_ERNO_TEST);
+                /* check test condition */
+        sol_assert (!mock_assertpass(), SOL_ERNO_TEST);
 
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      test_assert_02() - sol_assert() unit test #2
+ *      test_assert2() - sol_assert() unit test #2
  */
-static sol_erno
-test_assert_02(void)
+static sol_erno test_assert2(void)
 {
+        #define DESC_ASSERT2 "sol_assert() must throw an error for a" \
+                          " predicate that evaluates to false"
 SOL_TRY:
-                /* check test condition described by DESC_ASSERT_02 */
-        sol_assert (assert_fail (), SOL_ERNO_TEST);
+                /* check test condition */
+        sol_assert (mock_assertfail(), SOL_ERNO_TEST);
 
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      test_try_01() - sol_try() unit test #1
+ *      test_try1() - sol_try() unit test #1
  */
-static sol_erno
-test_try_01(void)
+static sol_erno test_try1(void)
 {
+        #define DESC_TRY1 "sol_try() must not throw an error for a function that" \
+                       " returns a SOL_ERNO_NULL error code"
 SOL_TRY:
-                /* check test condition described by DESC_TRY_01 */
-        sol_assert (!try_pass (), SOL_ERNO_TEST);
+                /* check test condition */
+        sol_assert (!mock_trypass(), SOL_ERNO_TEST);
 
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      test_try_02() - sol_try() unit test #2
+ *      test_try2() - sol_try() unit test #2
  */
-static sol_erno
-test_try_02(void)
+static sol_erno test_try2(void)
 {
+        #define DESC_TRY2 "sol_try() must throw an error for a function that" \
+                       " returns an error code other than SOL_ERNO_NULL"
 SOL_TRY:
-                /* check test condition described by DESC_TRY_01 */
-        sol_assert (try_fail (), SOL_ERNO_TEST);
+                /* check test condition */
+        sol_assert (mock_tryfail(), SOL_ERNO_TEST);
 
 SOL_CATCH:
 SOL_FINALLY:
                 /* wind up */
-        sol_throw ();
+        sol_throw();
 }
 
 
 
 
 /*
- *      test_now_01() - sol_erno_get() unit test #1
+ *      test_get1() - sol_erno_get() unit test #1
  */
-static sol_erno
-test_now_01(void)
+static sol_erno test_get1(void)
 {
+        #define DESC_GET1 "sol_erno_get() must return the current error code"
 SOL_TRY:
-                /* assert_fail () is guaranteed to fail with SOL_ERNO_TEST */
-        sol_try (assert_fail ());
+                /* set up test scenario */
+        sol_try (mock_assertfail());
 
 SOL_CATCH:
+                /* check test condition */
+        sol_erno_set(SOL_ERNO_TEST == sol_erno_get()
+                     ? SOL_ERNO_NULL
+                     : SOL_ERNO_TEST);
 SOL_FINALLY:
-                /* check test condition described by DESC_NOW_01 */
-        return SOL_ERNO_TEST == sol_erno_get ()
-               ? SOL_ERNO_NULL
-               : SOL_ERNO_TEST;
+                /* wind up */
+        sol_throw();
 }
 
 
@@ -255,10 +212,9 @@ SOL_FINALLY:
  */
 extern sol_erno
 __sol_tsuite_error(sol_tlog *log,
-                   int      *pass,
-                   int      *fail,
-                   int      *total
-                  )
+                   int *pass,
+                   int *fail,
+                   int *total)
 {
         auto sol_tsuite __ts, *ts = &__ts;
 
@@ -267,22 +223,22 @@ SOL_TRY:
         sol_assert (log && pass && fail && total, SOL_ERNO_PTR);
 
                 /* initialise test suite */
-        sol_try (sol_tsuite_init2 (ts, log));
+        sol_try (sol_tsuite_init2(ts, log));
 
                 /* register test cases */
-        sol_try (sol_tsuite_register (ts, test_assert_01, DESC_ASSERT_01));
-        sol_try (sol_tsuite_register (ts, test_assert_02, DESC_ASSERT_02));
-        sol_try (sol_tsuite_register (ts, test_try_01,    DESC_TRY_01));
-        sol_try (sol_tsuite_register (ts, test_try_02,    DESC_TRY_02));
-        sol_try (sol_tsuite_register (ts, test_now_01,    DESC_NOW_01));
+        sol_try (sol_tsuite_register(ts, test_assert1, DESC_ASSERT1));
+        sol_try (sol_tsuite_register(ts, test_assert2, DESC_ASSERT2));
+        sol_try (sol_tsuite_register(ts, test_try1, DESC_TRY1));
+        sol_try (sol_tsuite_register(ts, test_try2, DESC_TRY2));
+        sol_try (sol_tsuite_register(ts, test_get1, DESC_GET1));
 
                 /* execute test cases */
-        sol_try (sol_tsuite_exec (ts));
+        sol_try (sol_tsuite_exec(ts));
 
                 /* return test counts */
-        sol_try (sol_tsuite_pass  (ts, pass));
-        sol_try (sol_tsuite_fail  (ts, fail));
-        sol_try (sol_tsuite_total (ts, total));
+        sol_try (sol_tsuite_pass(ts, pass));
+        sol_try (sol_tsuite_fail(ts, fail));
+        sol_try (sol_tsuite_total(ts, total));
 
 SOL_CATCH:
 SOL_FINALLY:
