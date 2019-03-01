ef1422e14bdf7b63e7248a5d8cdd01f385a9366b
Abhishek Chakravarti <abhishek@taranjali.org>
2019-03-01 09:56:32 +0530

    Declare log file configuration identifiers
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ inc/config.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+ *                           SOL LIBRARY v0.1.0+41
+ *
+ * File: sol/inc/config.h
+ *
+ * Description:
+ *      This file is part of the API of the Sol Library. It declares the
+ *      configuration identifiers of the Sol Library.
+ *
+ * Authors:
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *
+ * Copyright:
+ *      (c) 2019 Abhishek Chakravarti
+ *      <abhishek@taranjali.org>
+ *
+ * License:
+ *      Released under the GNU General Public License version 3 (GPLv3)
+ *      <http://opensource.org/licenses/GPL-3.0>. See the accompanying LICENSE
+ *      file for complete licensing details.
+ *
+ *      BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT
+ *      YOU HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT THEM.
+ ******************************************************************************/
+
+
+
+
+        /* define header guard */
+#if (!defined __SOL_CONFIGURATION_IDENTIFIERS)
+#define __SOL_CONFIGURATION_IDENTIFIERS
+
+
+
+
+/*
+ *      SOL_CONFIG_ERRORLOG - path to error log file
+ */
+#define SOL_CONFIG_ERRORLOG "bld/error.log"
+
+
+
+
+/*
+ *      SOL_CONFIG_TRACELOG - path to trace log file
+ */
+#define SOL_CONFIG_TRACELOG "bld/trace.log"
+
+
+
+
+/*
+ *      SOL_CONFIG_DEBUGLOG - path to debug log file
+ */
+#define SOL_CONFIG_DEBUGLOG "bld/debug.log"
+
+
+
+
+#endif /* !defined __SOL_CONFIGURATION_IDENTIFIERS */
+
+
+
+
+/******************************************************************************
+ *                                    EOF
+ *          Built on hyperion [Tue Jan 29 02:37:24 UTC 2019]
+ ******************************************************************************/
+
