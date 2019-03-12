6d9086edee15084329ecc72c46d85c6ece645f11
Abhishek Chakravarti <abhishek@taranjali.org>
2019-03-12 13:53:00 +0530

    Define SOL_BOOL type
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ inc/prim.h
@@ -0,0 +1,74 @@
+/******************************************************************************
+ *                           SOL LIBRARY v0.1.0+41
+ *
+ * File: sol/inc/prim.h
+ *
+ * Description:
+ *      This file is part of the API of the Sol Library. It declares the
+ *      interface of the primitives module.
+ *
+ * Authors:
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *
+ * Copyright:
+ *      (c) 2019 Abhishek Chakravarti
+ *      <abhishek@taranjali.org>
+ *
+ * License:
+ *      Released under the GNU General Public License version 3 (GPLv3)
+ *      <http://opensource.org/licenses/GPL-3.0>. See the accompanying LICENSE
+ *      file for complete licensing details.
+ *
+ *      BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT
+ *      YOU HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT THEM.
+ ******************************************************************************/
+
+
+
+
+        /* define header guard */
+#if (!defined __SOL_PRIMITIVES_MODULE)
+#define __SOL_PRIMITIVES_MODULE
+
+
+
+
+        /* include required header files; the stdbool.h and stdint.h header
+         * files are used in case a C99 environment is available */
+#include "./env.h"
+#if (sol_env_stdc() >= SOL_ENV_STDC_C99)
+#       include <stdbool.h>
+#       include <stdint.h>
+#endif
+
+
+
+
+/*
+ * SOL_BOOL - a Boolean value
+ *   - SOL_BOOL_FALSE: Boolean value 'False'
+ *   - SOL_BOOL_TRUE: Boolean value 'True'
+ */
+#if (sol_env_stdc() >= SOL_ENV_STDC_C99)
+        typedef bool SOL_BOOL;
+#       define SOL_BOOL_FALSE (false)
+#       define SOL_BOOL_TRUE (true)
+#else
+        typedef int SOL_BOOL;
+#       define SOL_BOOL_FALSE ((SOL_BOOL) 0)
+#       define SOL_BOOL_TRUE ((SOL_BOOL) 1)
+#endif
+
+
+
+
+#endif /* (!defined __SOL_PRIMITIVES_MODULE) */
+
+
+
+
+/******************************************************************************
+ *                                    EOF
+ *          Built on hyperion [Tue Jan 29 02:37:24 UTC 2019]
+ ******************************************************************************/
+
