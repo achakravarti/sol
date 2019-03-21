7b8c62bbc8b4c82913ea8607c4eca14e5cc8baf8
Abhishek Chakravarti <abhishek@taranjali.org>
2019-03-21 20:34:39 +0530

    Add bool_test1()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/ts-prim.c
+++ test/ts-prim.c
@@ -33,7 +33,24 @@
 
 
 
-        /* implement the primitives module test suite */
+        /* bool_test1() implements the unit test described BOOL_TEST1 */
+static sol_erno bool_test1(void)
+{
+        #define BOOL_TEST1 "SOL_BOOL_TRUE must evaluate to 1"
+
+SOL_TRY:
+                /* check test condition */
+        sol_assert (SOL_BOOL_TRUE == 1, SOL_ERNO_TEST);
+
+SOL_CATCH:
+SOL_FINALLY:
+        return sol_erno_get();
+}
+
+
+
+
+        /* __sol_tests_prim() was declared in sol/test/suite.h */
 extern sol_erno __sol_tests_prim(sol_tlog *log,
                                  int *pass,
                                  int *fail,
@@ -49,6 +66,8 @@ SOL_TRY:
         sol_try (sol_tsuite_init2(ts, log));
 
                 /* register test cases */
+        sol_try (sol_tsuite_register(ts, bool_test1, BOOL_TEST1));
+
 
                 /* execute test cases */
         sol_try (sol_tsuite_exec(ts));
