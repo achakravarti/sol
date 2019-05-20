440db1c1e13787d771a9b165fdca8f99efba84f3
Abhishek Chakravarti <abhishek@taranjali.org>
2019-05-20 14:39:14 +0530

    Add meta_new2_test1() test case
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/ts-elem.c
+++ test/ts-elem.c
@@ -55,6 +55,36 @@ SOL_FINALLY:
 
 
 
+static void mock_dispose(sol_ptr **disp)
+{
+        (void) disp;
+}
+
+
+
+
+static sol_erno meta_new2_test1(void)
+{
+        #define META_NEW2_TEST1 "sol_elem_meta_new2() throws SOL_ERNO_PTR if" \
+                                " passed a null pointer for @meta"
+        const sol_index ID = (sol_index) 1;
+        const sol_index SZ = (sol_index) 1;
+
+SOL_TRY:
+        sol_try (sol_elem_meta_new2(SOL_PTR_NULL, ID, SZ, mock_dispose));
+
+SOL_CATCH:
+        sol_erno_set(sol_erno_get() == SOL_ERNO_PTR
+                     ? SOL_ERNO_NULL
+                     : SOL_ERNO_TEST);
+
+SOL_FINALLY:
+        return sol_erno_get();
+}
+
+
+
+
 extern sol_erno __sol_tests_elem(sol_tlog *log,
                                  sol_uint *pass,
                                  sol_uint *fail,
@@ -68,6 +98,7 @@ SOL_TRY:
         sol_try (sol_tsuite_init2(ts, log));
 
         sol_try (sol_tsuite_register(ts, meta_new_test1, META_NEW_TEST1));
+        sol_try (sol_tsuite_register(ts, meta_new2_test1, META_NEW_TEST1));
 
         sol_try (sol_tsuite_exec(ts));
         sol_try (sol_tsuite_pass(ts, pass));
