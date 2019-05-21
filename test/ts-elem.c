260c58774dd03844e6a2fb70dd2494a9c704fc06
Abhishek Chakravarti <abhishek@taranjali.org>
2019-05-21 07:05:44 +0530

    Add meta_new3_test4() test case
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/ts-elem.c
+++ test/ts-elem.c
@@ -250,6 +250,31 @@ SOL_FINALLY:
 
 
 
+static sol_erno meta_new3_test4(void)
+{
+        #define META_NEW3_TEST4 "sol_elem_meta_new3() throws SOL_ERNO_PTR" \
+                                " if passed a null pointer for @eq"
+        const sol_index ID = (sol_index) 1;
+        const sol_index SZ = (sol_index) 0;
+
+        auto sol_elem_meta *meta = SOL_PTR_NULL;
+
+SOL_TRY:
+        sol_try (sol_elem_meta_new3(&meta, ID, SZ, mock_dispose, SOL_PTR_NULL));
+
+SOL_CATCH:
+        sol_erno_set(sol_erno_get() == SOL_ERNO_PTR
+                     ? SOL_ERNO_NULL
+                     : SOL_ERNO_TEST);
+
+SOL_FINALLY:
+        sol_elem_meta_free(&meta);
+        return sol_erno_get();
+}
+
+
+
+
 static sol_erno meta_new4_test1(void)
 {
         #define META_NEW4_TEST1 "sol_elem_meta_new4() throws SOL_ERNO_PTR if" \
@@ -362,6 +387,7 @@ SOL_TRY:
         sol_try (sol_tsuite_register(ts, meta_new3_test1, META_NEW3_TEST1));
         sol_try (sol_tsuite_register(ts, meta_new3_test2, META_NEW3_TEST2));
         sol_try (sol_tsuite_register(ts, meta_new3_test3, META_NEW3_TEST3));
+        sol_try (sol_tsuite_register(ts, meta_new3_test4, META_NEW3_TEST4));
 
         sol_try (sol_tsuite_register(ts, meta_new4_test1, META_NEW4_TEST1));
         sol_try (sol_tsuite_register(ts, meta_new4_test2, META_NEW4_TEST2));
