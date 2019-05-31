33d0a23836bacfde8a3f80e90d32d28d2ebcac31
Abhishek Chakravarti <abhishek@taranjali.org>
2019-05-31 12:24:24 +0530

    Add eq_test4() test case
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/ts-elem.c
+++ test/ts-elem.c
@@ -693,6 +693,44 @@ SOL_FINALLY:
 
 
 
+        /* eq_test4() defines the test case described by EQ_TEST4 */
+static sol_erno eq_test4(void)
+{
+        #define EQ_TEST4 "sol_elem_eq() throws SOL_ERNO_STATE if @lhs" \
+                         " and @rhs are of different types"
+        auto sol_elem_meta *meta1 = SOL_PTR_NULL;
+        auto sol_elem_meta *meta2 = SOL_PTR_NULL;
+        auto sol_elem *elem1 = SOL_PTR_NULL;
+        auto sol_elem *elem2 = SOL_PTR_NULL;
+        auto sol_int data = (sol_int) 5;
+        auto SOL_BOOL eq;
+
+SOL_TRY:
+                /* set up test */
+        sol_try (meta_new(&meta1));
+        sol_try (meta_new2(&meta2));
+        sol_try (sol_elem_new(&elem1, meta1, (sol_ptr*) &data));
+        sol_try (sol_elem_new(&elem2, meta2, (sol_ptr*) &data));
+        sol_try (sol_elem_lt(elem1, elem2, &eq));
+
+SOL_CATCH:
+                /* check test condition */
+        sol_erno_set(sol_erno_get() == SOL_ERNO_STATE
+                     ? SOL_ERNO_NULL
+                     : SOL_ERNO_TEST);
+
+SOL_FINALLY:
+                /* tear down test */
+        sol_elem_meta_free(&meta1);
+        sol_elem_meta_free(&meta2);
+        sol_elem_free(&elem1);
+        sol_elem_free(&elem2);
+        return sol_erno_get();
+}
+
+
+
+
         /* __sol_tests_elem() was declared in sol/test/suite.h */
 extern sol_erno __sol_tests_elem(sol_tlog *log,
                                  sol_uint *pass,
@@ -738,6 +776,7 @@ SOL_TRY:
         sol_try (sol_tsuite_register(ts, eq_test1, EQ_TEST1));
         sol_try (sol_tsuite_register(ts, eq_test2, EQ_TEST2));
         sol_try (sol_tsuite_register(ts, eq_test3, EQ_TEST3));
+        sol_try (sol_tsuite_register(ts, eq_test4, EQ_TEST4));
 
                 /* execute test cases */
         sol_try (sol_tsuite_exec(ts));
